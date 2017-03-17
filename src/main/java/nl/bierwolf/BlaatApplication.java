132f5610dacfc6f8025d44cdacc816c4d046c0d6
André Bierwolf <a.b.bierwolf@gmail.com>
2017-03-17 13:21:27 +0100

    First project commit

---
+++ src/main/java/nl/bierwolf/BlaatApplication.java
@@ -0,0 +1,12 @@
+package nl.bierwolf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlaatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlaatApplication.class, args);
+	}
+}
